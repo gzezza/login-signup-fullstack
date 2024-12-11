30f7d0b00ae80206252a8d70fa14f999a0d3e05d
gzezza <guilhermezezza@gmail.com>
2024-12-11 10:58:00 -0300

    feat: creating initial structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
+import { LoginComponent } from './pages/login/login.component';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+    path: "login",
+    component: LoginComponent
+    }
+];
